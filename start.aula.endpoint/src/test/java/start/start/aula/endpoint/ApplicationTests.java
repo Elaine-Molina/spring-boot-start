7aea64757f7588c9f886346b09882dfeddb63169
Elaine.Molina <elaine.molina@capgemini.com>
2022-10-21 15:09:36 -0300

    Introdução/Criando Projeto

---
+++ start.aula.endpoint/src/test/java/start/start/aula/endpoint/ApplicationTests.java
@@ -0,0 +1,13 @@
+package start.start.aula.endpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
